2c8b3ecf63699bca2dce9ca4f5a2317c02876be4
Thiago Batista <thiago.batista41@etec.sp.gov.br>
2025-05-08 13:42:38 -0300

    corrigindo erro

--- exibir_nome.c
+++ exibir_nome.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+void main(){
 char nome[50];
 printf("\nDigite seu nome abaixo por favor: \n");
 fgets(nome, sizeof(nome), stdin);
 nome[strcspn(nome, "\n")]= '\0';
 printf("\nOla, %s, Muito Prazer!\n", nome);
 system("pause");
+}
