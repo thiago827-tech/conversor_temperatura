9f44381d86ef69e1e5d99a7e9d48bad03697e46c
Thiago Batista <thiago.batista41@etec.sp.gov.br>
2025-05-08 13:18:45 -0300

    corrigindo problema na formula e exibicao

--- conversor.c
+++ conversor.c
@@ -5,7 +5,7 @@ float c, f;
 printf("\nBem vindo ao programa de conversao de temperatura!\n");
 printf("\nInforme o valor da temperatura em celsius que deseja converter para fahrenheit: \n");
 scanf("%f", &c);
-f=(c*1.8)-32;
-printf("\nO valor da temperatura e: %.1f F°\n", f);
+f=(c*1.8)+32;
+printf("\nO valor da temperatura e: %.1f Fahrenheit\n", f);
 printf("\nObrigado por testar o programa :)\n");
 }
