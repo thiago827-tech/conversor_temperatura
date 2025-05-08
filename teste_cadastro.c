ba66c43b1365d6cfe7f654cfe11189c9cf1db7b7
Thiago Batista <thiago.batista41@etec.sp.gov.br>
2025-05-08 15:19:10 -0300

    bug resolvido

--- teste_cadastro.c
+++ teste_cadastro.c
@@ -5,24 +5,25 @@ int main() {
   int qtd;
   char nomes[10][100];
   float notas[10];
-  printf("Quantos alunos deseja cadastrar? (max 10)");
+  printf("\nQuantos alunos deseja cadastrar? (max 10)\n");
   scanf("%d", &qtd);
   getchar();
   if(qtd< 1 || qtd > 10){
-    printf("Quantidade invalida, deve ser entre 1 e  10\n");
+    printf("\nQuantidade invalida, deve ser entre 1 e  10\n");
     return 1;
   }
-  for(int i = 0; i < qtd; i++){
-    printf("Digite o nome do aluno %d: ", i + 1);
+  int i;
+  for (i = 0; i < qtd; i++){
+    printf("\nDigite o nome do aluno %d: \n", i + 1);
     fgets(nomes[i], sizeof(nomes[i]), stdin);
     nomes[i][strcspn(nomes[i], "\n")]= '\0';
-    printf("Digite a nota  do aluno %d: ", i+1);
+    printf("\nDigite a nota  do aluno %d: \n", i+1);
     scanf("%f", &notas[i]);
     getchar();
   }
   printf("\nAlunos Cadastrados: \n");
-  for(int i = 0 < qtd; i++){
-    printf("Aluno %d: %s - Nota: %.2f\n", i + 1, nomes[i], notas[i]);
+  for(i = 0; i < qtd; i++){
+    printf("\nAluno %d: %s - Nota: %.2f\n", i + 1, nomes[i], notas[i]);
   }
   return 0;
 }
