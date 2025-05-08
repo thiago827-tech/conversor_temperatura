f516c88346fbeb14ec890b515d84c0cf5a374ea6
Thiago <thiago.batista41@etec.sp.gov.br>
2025-05-08 13:14:19 -0300

    correção de bug

--- conversor.c
+++ conversor.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 void main(){
 float c, f;
-printf("/nBem vindo ao programa de conversao de temperatura!/n");
-printf("/nInforme o valor da temperatura em celsius que deseja converter para fahrenheit: /n");
+printf("\nBem vindo ao programa de conversao de temperatura!\n");
+printf("\nInforme o valor da temperatura em celsius que deseja converter para fahrenheit: \n");
 scanf("%f", &c);
 f=(c*1.8)-32;
-printf("/nO valor da temperatura e: %.1f F°/n", f);
-printf("/nObrigado por testar o programa :)/n");
+printf("\nO valor da temperatura e: %.1f F°\n", f);
+printf("\nObrigado por testar o programa :)\n");
 }
